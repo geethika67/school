675089bba49ca477835b8e0e835e3c9c3d56011a
geethika67 <geethikam30@gmail.com>
2022-10-27 11:54:40 +0530

    1st commit

---
+++ students/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('studentshome',views.studenthome),
+    path('css',views.css),
+    path('css2',views.css2),
+    path('css3',views.css3),
+    path('css4',views.css4),
+    path('css5',views.css5),
+    path('css6',views.css6),
+    path('css7',views.css7),
+   
+
+
+]
---
+++ students/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def studenthome(request):
+    return render(request,'students/studentshome.html')
+
+
+def css(request):
+    return render(request,'students/css.html')
+
+
+def css2(request):
+    return render(request,'students/css2.html')
+
+
+def css3(request):
+    return render(request,'students/css3.html')
+
+
+def css4(request):
+    return render(request,'students/css4.html')
+
+
+def css5(request):
+    return render(request,'students/css5.html')
+
+
+def css6(request):
+    return render(request,'students/css6.html')
+
+
+def css7(request):
+    return render(request,'students/css7.html')
+    
+
+
+
+    
+    
+    
