1d7a1331af884c4c2d469c5ec10522b0970fd0e0
geethika67 <geethikam30@gmail.com>
2022-10-27 12:12:31 +0530

    2nd commit

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3c^+n(_$(==-@$#0%=no$cq4@mrgi%z1rcg(_uc*n%khwg^c6a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
